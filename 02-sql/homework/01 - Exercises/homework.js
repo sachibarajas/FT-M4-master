47fbabc353b42b425ee7e17abc8376a1c0c7aef3
Santiago Barajas <barajas03@gmail.com>
2023-03-10 18:43:07 -0500

    SQL done

--- 02-sql/homework/01 - Exercises/homework.js
+++ 02-sql/homework/01 - Exercises/homework.js
@@ -1,14 +1,14 @@
 /* ¡Escribe tus comandos en este archivo! */
 
-const ejercicio02 = '';
+const ejercicio02 = 'SELECT * FROM movies WHERE duration < 90;';
 
-const ejercicio03 = '';
+const ejercicio03 = 'SELECT * FROM movies WHERE year<=1940 AND year>=1935;';
 
-const ejercicio04 = '';
+const ejercicio04 = 'SELECT * FROM movies WHERE title LIKE "%til%"';
 
-const ejercicio05 = '';
+const ejercicio05 = 'SELECT * FROM movies WHERE cardinality(actors)=1;';
 
-const ejercicio06 = '';
+const ejercicio06 = 'SELECT title, AVG(rating) FROM movies, unnest(ratings)rating GROUP BY title;';
 
 const ejercicio07 = '';
 
